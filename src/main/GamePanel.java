dfd13e75788adcabef570445daee5cfbe65b956c
milchmek <pongkhun.mek@gmail.com>
2024-12-01 16:23:32 +0700

    p[layer

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,4 +1,82 @@
 package main;
 
-public class GamePanel {
+import entity.Player;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    public final int originalTileSize = 16;
+    public final int scale = 3;
+    public final int tileSize = originalTileSize  * scale;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = maxScreenCol  * tileSize;
+    public final int screenHeight = maxScreenRow * tileSize;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(new Color(0x222222));
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
+    }
+
+    Thread gameThread;
+    KeyHandler keyHandler = new KeyHandler();
+    Player player = new Player();
+
+    final int FPS = 60;
+
+    int playerX = 100;
+    int playerY = 100;
+    int speed = 3;
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = (double) 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    public void update() {
+        if (keyHandler.upPressed) {
+            playerY -= speed;
+        }
+        if (keyHandler.leftPressed) {
+            playerX -= speed;
+        }
+        if (keyHandler.rightPressed) {
+            playerX += speed;
+        }
+        if (keyHandler.downPressed) {
+            playerY += speed;
+        }
+    }
+
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        Graphics2D graphics2D = (Graphics2D) graphics;
+        graphics2D.setColor(new Color(0xeeeeee));
+        graphics2D.fillRect(playerX, playerY, tileSize, tileSize);
+        graphics2D.dispose();
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,20 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Try Another Time to create a game using java");
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GamePanel gamePanel = new GamePanel();
+        jFrame.add(gamePanel);
+        jFrame.pack();
+
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
 }
